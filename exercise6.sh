aeecb629daa8c235b219acc83e385ffaad567a27
vegariy77 <vegariy@mail.ru>
2026-02-16 09:13:35 +0300

    Update exercise6.sh

--- exercise6.sh
+++ exercise6.sh
@@ -1,7 +1,7 @@
 echo "Привет"
 echo "Мое имя" $USER
-echo "Сейчас я нахожусь в" $HOME
 echo "Сейчас я нахожусь в" $PWD
+echo "Мой домашний каталог" $HOME
 echo "Моя OC" 
 cat /etc/os-release
 echo "Мое время работы"
